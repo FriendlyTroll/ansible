e2e0d833be103b2d919f103540e54cfb9f03f39e
antisa <antisa@pm.me>
2023-10-10 12:37:15 +0200

    ubuntu-setup: update tmux scripts

--- roles/ubuntu-setup/files/tmux-scripts/tmux-script-int.sh
+++ roles/ubuntu-setup/files/tmux-scripts/tmux-script-int.sh
@@ -3,7 +3,12 @@
 SESSION_NAME=int
 CHANGE_TO_DIR=/home/antisa/Posal/ansible/
 WINDOW_1=ansible-int
+WINDOW_2=deploy
+WINDOW_3=vagrant
 
 tmux new-session -d -s ${SESSION_NAME} -n ${WINDOW_1} -c ${CHANGE_TO_DIR}
 tmux send-keys -t ${SESSION_NAME}:${WINDOW_1} "vim" Enter
+tmux new-window -n ${WINDOW_2}
+tmux new-window -n ${WINDOW_3}
+tmux send-keys -t ${SESSION_NAME}:${WINDOW_3} "cdv" Enter
 asciinema rec $HOME/Documents/terminal_logs/$(date +"%F_%H-%M-%S").cast -c "tmux attach -t ${SESSION_NAME}:${WINDOW_1}"
