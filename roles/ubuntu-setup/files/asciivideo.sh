a3360476c230e42ec9e807c350e488e1409c01c0
antisa <antisa@pm.me>
2023-11-29 16:33:22 +0100

    ubuntu-setup: update asciinema.sh script

--- roles/ubuntu-setup/files/asciivideo.sh
+++ roles/ubuntu-setup/files/asciivideo.sh
@@ -4,6 +4,14 @@
 # Get file selected
 FILES=($NAUTILUS_SCRIPT_SELECTED_FILE_PATHS)
 FILES_COUNT=${#FILES[@]}
+PYTON_SERVER_PORT=8015
+
+# run python built in webserver
+if lsof -Pi :$PYTON_SERVER_PORT -sTCP:LISTEN -t >/dev/null ; then
+    echo "Server already running"
+else
+    python3 -m http.server --directory /home/antisa/Documents/terminal_logs/ --bind 127.0.0.1 $PYTON_SERVER_PORT &
+fi
 
 
 if [ $FILES_COUNT -gt 0 ]
@@ -14,14 +22,13 @@ then
         (# get timestamp from filename
         newf=$(echo "$file" | cut -f6 -d"/" | cut -f1 -d".")
         # get string to substitute
-        asciiname=$(awk -F[\'\'] '/Ascii/ {print $2}' asciinema_player.html | tr -d "/" | cut -f1 -d".") 
+        asciiname=$(awk -F[\'\'] '/Ascii/ {print $2}' asciinema_player.html | tr -d "/" | cut -f1 -d".")
         # set correct string in asciinema html file
         sed -i "s/$asciiname/$newf/" /home/antisa/Documents/terminal_logs/asciinema_player.html
         sleep 1
-        # run python built in webbrowser and open in firefox
-        python3 -m http.server
-        sleep 1
-        firefox http://0.0.0.0:8000/asciinema_player.html
+        # open in firefox
+        firefox http://127.0.0.1:$PYTON_SERVER_PORT/asciinema_player.html
         ) | zenity --progress --width 350 --pulsate --text "Opening recording..." --title "asciinema recording" --auto-close
     done
 fi
+
