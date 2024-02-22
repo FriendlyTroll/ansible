24616d9bbb4f9d4f5797dcd503b57d659b84daf5
antisa <antisa@pm.me>
2024-02-22 16:40:45 +0100

    neovim: add more telescope shortcuts

--- roles/neovim/files/nvim/lua/plugins/telescope.lua
+++ roles/neovim/files/nvim/lua/plugins/telescope.lua
@@ -37,15 +37,22 @@ return {
 	config = config,
 	keys = {
 		vim.keymap.set("n", "<leader>fk", ":Telescope keymaps<CR>"),
-		vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>"),
+		vim.keymap.set("n", "<leader>fh", ":Telescope command_history<CR>"),
 		vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>"),
 		vim.keymap.set("n", "<leader>fs", ":Telescope find_files search_dirs=/home,/etc<CR>"),
 		vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>"),
-		vim.keymap.set("n", "<leader>fl", ":Telescope grep_string<CR>"),
+		vim.keymap.set(
+			"n",
+			"<leader>fw",
+			":Telescope grep_string<CR>",
+			{ desc = "Search for word in entire project when selected in visual mode " }
+		),
 		vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>"),
 		vim.keymap.set("n", "<leader>fo", ":Telescope oldfiles<CR>"),
 		vim.keymap.set("n", "<leader>fc", ":AdvancedGitSearch search_log_content<CR>"),
 		vim.keymap.set("n", "<leader>fbc", ":AdvancedGitSearch search_log_content_file<CR>"),
 		vim.keymap.set("n", "<leader>fr", ":Telescope registers<CR>"),
+		vim.keymap.set("n", "<leader>fl", ":Telescope current_buffer_fuzzy_find<CR>"),
+		vim.keymap.set("n", "<leader>fm", ":Telescope marks<CR>", { desc = "Search for marks" }),
 	},
 }
