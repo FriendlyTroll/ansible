a7c590149e61daccfc7bf33d7be21e10d0ced8ee
antisa <antisa@pm.me>
2024-02-26 12:21:42 +0100

    neovim: update advanced-git-search config

--- roles/neovim/files/nvim/lua/plugins/advanced-git-search.lua
+++ roles/neovim/files/nvim/lua/plugins/advanced-git-search.lua
@@ -9,7 +9,18 @@ return {
 		-- optional: setup telescope before loading the extension
 		require("telescope").setup({
 			extensions = {
-				advanced_git_search = {},
+				advanced_git_search = {
+					telescope_theme = {
+						search_log_content = {
+							layout_config = { width = 0.8, height = 0.8 },
+							layout_strategy = "vertical",
+						},
+						search_log_content_file = {
+							layout_config = { width = 0.8, height = 0.8 },
+							layout_strategy = "vertical",
+						},
+					},
+				},
 			},
 		})
 
