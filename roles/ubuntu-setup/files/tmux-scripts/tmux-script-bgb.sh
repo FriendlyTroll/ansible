c75e419c0ad9d3cad0519af838e6c357e6f45cd9
antisa <antisa@pm.me>
2024-03-01 11:21:52 +0100

    ubuntu-setup: remove unused tmux-scripts

--- roles/ubuntu-setup/files/tmux-scripts/tmux-script-bgb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# called by alias from .bashrc
-SESSION_NAME=bgb
-CHANGE_TO_DIR=/home/antisa/Posal/bgb.ansible/
-WINDOW_1=ansible-bgb
-
-tmux new-session -d -s ${SESSION_NAME} -n ${WINDOW_1} -c ${CHANGE_TO_DIR}
-tmux send-keys -t ${SESSION_NAME}:${WINDOW_1} "nvim" Enter
-asciinema rec $HOME/Documents/terminal_logs/$(date +"%F_%H-%M-%S").cast -c "tmux attach -t ${SESSION_NAME}:${WINDOW_1}"
