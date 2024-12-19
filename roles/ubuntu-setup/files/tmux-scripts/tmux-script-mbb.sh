779d5c0c05b3385207491100e959cd609d062b48
antisa <antisa@pm.me>
2024-12-19 16:57:05 +0100

    Update mbb tmux script

--- roles/ubuntu-setup/files/tmux-scripts/tmux-script-mbb.sh
+++ roles/ubuntu-setup/files/tmux-scripts/tmux-script-mbb.sh
@@ -5,30 +5,26 @@ SSH_CMD=/usr/bin/ssh
 ansible_sessions=(
   int
   de
-  bgb
 )
 ansible_dirs=(
   /home/antisa/Posal/ansible/
   /home/antisa/Posal/ansbile-2.0/
-  /home/antisa/Posal/bgb.ansible/
 )
 ansible_windows=(
   ansible-i18n
   ansbile-2-0
-  bgb-ansible
 )
 deploy_windows=(
   ansible-i18n
   ansbile-2.0
-  bgb-ansible
 )
 other_sessions=(
   vagrant
-  sty
+  icg
 )
 other_dirs=(
   /home/antisa/vagrant_playground/
-  /home/antisa/Posal/Stylight/Code
+  /home/antisa/Posal/icg/
 )
 
 for index in ${!ansible_sessions[*]};do
@@ -44,7 +40,10 @@ done
 
 tmux send-keys -t int:deploy "$SSH_CMD provisioning-i18n" Enter
 tmux send-keys -t de:deploy "$SSH_CMD provisioning" Enter
-tmux send-keys -t sty "export AWS_PROFILE=stylight" Enter
+
+tmux new-window -t icg: -n icg-infra-k8s-apps -c "/home/antisa/Posal/icg/icg-infra-k8s-apps"
+tmux send-keys -t icg:icg-infra-k8s-apps "vim" Enter
+tmux set-environment -t icg AWS_PROFILE icg
 
 # record the tmux sessions
 asciinema rec $HOME/Documents/terminal_logs/$(date +"%F_%H-%M-%S").cast -c "tmux attach -t ${ansible_sessions[0]}:${ansible_windows[0]}"
