64d30f233ded9b9e5b3695e8486348fc4e7c3a01
antisa <antisa@pm.me>
2025-03-31 11:22:02 +0200

    ubuntu-setup: update tmux-script

--- roles/ubuntu-setup/files/tmux-scripts/tmux-script-mbb.sh
+++ roles/ubuntu-setup/files/tmux-scripts/tmux-script-mbb.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # called by alias from .bashrc
+#
+# debug
+# set -x
+
 SSH_CMD=/usr/bin/ssh
 
 # If an argument is given, VIM_CMD is set to vim +colorscheme $1
@@ -8,49 +12,45 @@ if [ -n "$1" ]; then
       VIM_CMD="vim +'colorscheme $1'"
 fi
 
-ansible_sessions=(
-  int
-  de
-)
-ansible_dirs=(
-  /home/antisa/Posal/ansible/
-  /home/antisa/Posal/ansbile-2.0/
-)
-ansible_windows=(
-  ansible-i18n
-  ansbile-2-0
-)
-deploy_windows=(
-  ansible-i18n
-  ansbile-2.0
-)
-other_sessions=(
-  vagrant
-  icg
-)
-other_dirs=(
-  /home/antisa/vagrant_playground/
-  /home/antisa/Posal/icg/
+# Define your sessions and directories here
+# Format: "session_name:dir1,dir2,..."
+declare -A sessions=(
+    ["de"]="/home/antisa/Posal/ansbile-2.0/"
+    ["int"]="/home/antisa/Posal/ansible/"
+    ["icg"]="/home/antisa/Posal/icg/icg-web/,/home/antisa/Posal/icg/icg-infra-shared-ts/,/home/antisa/Posal/icg/icg-infra-global/"
+    ["vagrant"]="/home/antisa/vagrant_playground/"
 )
 
-for index in ${!ansible_sessions[*]};do
-  tmux new-session -d -s ${ansible_sessions[$index]} -n ${ansible_windows[$index]} -c ${ansible_dirs[$index]}
-  tmux new-window -n deploy -c "${ansible_dirs[$index]}"
-  tmux send-keys -t ${ansible_sessions[$index]}:${ansible_windows[$index]} "$VIM_CMD" Enter
-  tmux select-window -t 0
+# Function to check if a tmux session exists
+session_exists() {
+    tmux has-session -t "$1" 2>/dev/null
+}
+# Loop through the sessions and create them and their windows
+for session_name in "${!sessions[@]}"; do
+    dirs_string="${sessions[$session_name]}"  # Extract directories string
+    IFS=',' read -r -a dirs <<< "$dirs_string"  # Split into array
+
+    if session_exists "$session_name"; then
+        echo "Session '$session_name' already exists. Skipping..."
+        continue
+    fi
+
+    # echo "Creating session: $session_name"
+    tmux new-session -d -s "$session_name" -c "${dirs[0]}"
+    tmux rename-window -t "$session_name":0 "$(basename "${dirs[0]}")"
+
+    # Create additional windows for multiple directories
+    index=1
+    for dir in "${dirs[@]:1}"; do
+        tmux new-window -t "$session_name":$index -c "$dir" -n "$(basename "$dir")"
+        ((index++))
+    done
 done
 
-for index in ${!other_sessions[*]};do
-  tmux new-session -d -s ${other_sessions[$index]} -c ${other_dirs[$index]}
-done
-
-tmux send-keys -t int:deploy "$SSH_CMD provisioning-i18n" Enter
-tmux send-keys -t de:deploy "$SSH_CMD provisioning" Enter
-
-tmux new-window -t icg: -n icg-infra-k8s-apps -c "/home/antisa/Posal/icg/icg-infra-k8s-apps"
-tmux send-keys -t icg:icg-infra-k8s-apps "$VIM_CMD" Enter
-tmux set-environment -t icg AWS_PROFILE icg
+# tmux new-window -t icg: -n icg-infra-k8s-apps -c "/home/antisa/Posal/icg/icg-infra-k8s-apps"
+# tmux send-keys -t icg:icg-infra-k8s-apps "$VIM_CMD" Enter
+# tmux set-environment -t icg AWS_PROFILE icg
 
 # record the tmux sessions
-asciinema rec $HOME/Documents/terminal_logs/$(date +"%F_%H-%M-%S").cast -c "tmux attach -t ${ansible_sessions[0]}:${ansible_windows[0]}"
+asciinema rec $HOME/Documents/terminal_logs/$(date +"%F_%H-%M-%S").cast -c "tmux attach -t 'icg'"
 
